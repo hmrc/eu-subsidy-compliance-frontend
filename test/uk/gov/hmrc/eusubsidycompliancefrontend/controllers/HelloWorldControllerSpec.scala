39294d0204ef900ec9bb89f23faea1f9b4961380
paulthor <3472481+PaulThor@users.noreply.github.com>
2021-10-12 13:36:27 +0100

    Fixed unit tests

--- test/uk/gov/hmrc/eusubsidycompliancefrontend/controllers/HelloWorldControllerSpec.scala
+++ test/uk/gov/hmrc/eusubsidycompliancefrontend/controllers/HelloWorldControllerSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.eusubsidycompliancefrontend.controllers
 
 import org.scalatest.matchers.should.Matchers
@@ -5,11 +21,13 @@ import org.scalatest.wordspec.AnyWordSpec
 import org.scalatestplus.play.guice.GuiceOneAppPerSuite
 import play.api.Application
 import play.api.http.Status
+import play.api.inject.guice.GuiceApplicationBuilder
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
-import play.api.inject.guice.GuiceApplicationBuilder
+import uk.gov.hmrc.eusubsidycompliancefrontend.actions.AuthenticatedActionBuilders
+import uk.gov.hmrc.eusubsidycompliancefrontend.views.html.HelloWorldPage
 
-class HelloWorldControllerSpec extends AnyWordSpec with Matchers with GuiceOneAppPerSuite {
+class HelloWorldControllerSpec extends AnyWordSpec with Matchers with GuiceOneAppPerSuite with AuthenticatedActionBuilders {
   override def fakeApplication(): Application =
     new GuiceApplicationBuilder()
       .configure(
@@ -19,8 +37,13 @@ class HelloWorldControllerSpec extends AnyWordSpec with Matchers with GuiceOneAp
       .build()
 
   private val fakeRequest = FakeRequest("GET", "/")
+  private val template = app.injector.instanceOf[HelloWorldPage]
 
-  private val controller = app.injector.instanceOf[HelloWorldController]
+  private val controller = new HelloWorldController(
+    stubMessagesControllerComponents(),
+    template,
+    preAuthenticatedActionBuilders
+  )
 
   "GET /" should {
     "return 200" in {
