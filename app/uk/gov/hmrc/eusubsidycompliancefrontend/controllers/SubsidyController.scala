1afcbf02faa2514f4ab0e1017533fe5bf75844ca
Sam Miele <9150276+sam-miele@users.noreply.github.com>
2022-01-24 15:35:58 +0000

    fix invalid validation

--- app/uk/gov/hmrc/eusubsidycompliancefrontend/controllers/SubsidyController.scala
+++ app/uk/gov/hmrc/eusubsidycompliancefrontend/controllers/SubsidyController.scala
@@ -338,7 +338,6 @@ class SubsidyController @Inject()(
   lazy val claimAmountForm : Form[BigDecimal] = Form( mapping("claim-amount" -> bigDecimal)(identity)(Some(_)))
 
   lazy val claimDateForm : Form[DateFormValues] = Form(
-    "claim-date" ->
     DateFormValues.vatRegDateMapping
       .verifying("error.date.invalid", a =>  a.isValidDate)
   )
