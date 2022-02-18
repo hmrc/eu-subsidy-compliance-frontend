d79f14549001749ef99fbeba6722481cf5dc80dd
Priyanka Dhiman <88194246+priyankadhiman3006@users.noreply.github.com>
2022-02-18 11:28:15 +0000

    ESC-256 update email

--- app/uk/gov/hmrc/eusubsidycompliancefrontend/controllers/UpdateEmailAddressController.scala
+++ app/uk/gov/hmrc/eusubsidycompliancefrontend/controllers/UpdateEmailAddressController.scala
@@ -20,22 +20,29 @@ import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.eusubsidycompliancefrontend.actions.EscActionBuilders
 import uk.gov.hmrc.eusubsidycompliancefrontend.config.AppConfig
 import uk.gov.hmrc.eusubsidycompliancefrontend.views.html._
+import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
 
 import javax.inject.{Inject, Singleton}
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.Future
 
 @Singleton
 class UpdateEmailAddressController @Inject()(
                                               mcc: MessagesControllerComponents,
-                                              updateEmailAddressPage: UpdateEmailAddressPage,
-                                              escActionBuilders: EscActionBuilders
-                                            )( implicit val appConfig: AppConfig,
-                                               executionContext: ExecutionContext) extends
+                                              updateEmailAddressPage: UpdateEmailPage,
+                                              escActionBuilders: EscActionBuilders,
+                                              servicesConfig: ServicesConfig
+                                            )( implicit val appConfig: AppConfig) extends
   BaseController(mcc) {
   import escActionBuilders._
 
-  def updateEmailAddress: Action[AnyContent] = escAuthentication.async { _ =>
+  def updateEmailAddress: Action[AnyContent] = escAuthentication.async { implicit request =>
     Future.successful(Ok(updateEmailAddressPage()))
+  }
+
+  def postUpdateEmailAddress: Action[AnyContent] = escAuthentication.async { _ =>
+     val baseUrl: String = servicesConfig.baseUrl("update-email")
+    val updatedEmailUrl: String = s"$baseUrl/manage-email-cds/service/eu-subsidy-compliance-frontend"
+    Future.successful(Redirect(updatedEmailUrl))
   }
 
   }
---
+++ test/uk/gov/hmrc/eusubsidycompliancefrontend/controllers/UpdateEmailAddressControllerSpec.scala
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.eusubsidycompliancefrontend.controllers
+
+import com.typesafe.config.ConfigFactory
+import play.api.Configuration
+import play.api.inject.bind
+import play.api.test.FakeRequest
+import uk.gov.hmrc.auth.core.AuthConnector
+import uk.gov.hmrc.eusubsidycompliancefrontend.services.Store
+import utils.CommonTestData.eori1
+
+import scala.concurrent.Future
+
+class UpdateEmailAddressControllerSpec extends ControllerSpec
+  with AuthSupport
+  with JourneyStoreSupport
+  with AuthAndSessionDataBehaviour {
+
+  override def overrideBindings           = List(
+    bind[AuthConnector].toInstance(mockAuthConnector),
+    bind[Store].toInstance(mockJourneyStore)
+  )
+
+  override def additionalConfig = super.additionalConfig.withFallback(
+    Configuration(
+      ConfigFactory.parseString(
+        s"""
+           | microservice.services.update-email {
+           |       protocol = http
+           |       host = localhost
+           |       port = 9898
+           |    }
+           |""".stripMargin)
+    )
+  )
+
+  val controller = instanceOf[UpdateEmailAddressController]
+
+  "UpdateEmailAddressController" when {
+
+    "handling request to update Email Address " must {
+
+      def performAction() = controller.updateEmailAddress(FakeRequest())
+      "display the page" in {
+        inSequence {
+          mockAuthWithNecessaryEnrolment()
+        }
+        checkPageIsDisplayed(
+          performAction(),
+          messageFromMessageKey("updateEmail.title"),
+          { doc =>
+            val button = doc.select("form")
+            button.attr("action") shouldBe routes.UpdateEmailAddressController.postUpdateEmailAddress().url
+
+          }
+        )
+
+      }
+    }
+
+    "handling request to post update email address" must {
+      def performAction() = controller.postUpdateEmailAddress(FakeRequest())
+      "redirect to next page" in {
+        inSequence {
+          mockAuthWithNecessaryEnrolment()
+        }
+        val url = "http://localhost:9898/manage-email-cds/service/eu-subsidy-compliance-frontend"
+        checkIsRedirect(performAction(), url )
+      }
+    }
+  }
+
+}
