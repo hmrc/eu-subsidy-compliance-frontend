6d6dc4df7d96abb2015f7e4e59ef5a4f1437a574
Priyanka Dhiman <88194246+priyankadhiman3006@users.noreply.github.com>
2022-03-11 16:07:37 +0000

    ESC-433 update dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,22 +4,23 @@ import sbt.Keys.libraryDependencies
 import sbt._
 
 object AppDependencies {
-  val bootStrapVersion = "5.18.0"
+  val bootStrapVersion = "5.20.0"
+  val hmrcMongoVersion = "0.60.0"
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % bootStrapVersion,
-    "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "1.31.0-play-28",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.53.0",
-    "org.typelevel"           %% "cats-core"                  % "2.7.0",
-    "ai.x"                    %% "play-json-extensions"       % "0.42.0",
-    "com.chuusai"             %% "shapeless"                  % "2.3.7"
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootStrapVersion,
+    "uk.gov.hmrc" %% "play-frontend-hmrc" % "3.5.0-play-28",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % hmrcMongoVersion,
+    "org.typelevel" %% "cats-core" % "2.7.0",
+    "ai.x" %% "play-json-extensions" % "0.42.0",
+    "com.chuusai" %% "shapeless" % "2.3.8"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % bootStrapVersion    % Test,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % "0.53.0"            % Test,
-    "org.jsoup"               %  "jsoup"                      % "1.13.1"            % Test,
-    "com.vladsch.flexmark"    %  "flexmark-all"               % "0.36.8"            % "test, it",
-    "org.scalatestplus"       %% "mockito-3-4"                % "3.2.7.0"           % "test, it",
-    "org.scalamock"           %% "scalamock"                  % "5.2.0"             % Test
+    "uk.gov.hmrc" %% "bootstrap-test-play-28" % bootStrapVersion % Test,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-28" % "0.53.0" % Test,
+    "org.jsoup" % "jsoup" % "1.13.1" % Test,
+    "com.vladsch.flexmark" % "flexmark-all" % "0.36.8" % "test, it",
+    "org.scalatestplus" %% "mockito-3-4" % "3.2.7.0" % "test, it",
+    "org.scalamock" %% "scalamock" % "5.2.0" % Test
   )
 }
