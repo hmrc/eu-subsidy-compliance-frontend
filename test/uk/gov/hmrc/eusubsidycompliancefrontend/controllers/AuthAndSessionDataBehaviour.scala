93700585ee234f6699e653b5d4a80d8dffea3db1
Carwyn Ellis <carwynellis@gmail.com>
2022-11-02 15:38:19 +0000

    ESC-772 resolve remaining test failures

--- test/uk/gov/hmrc/eusubsidycompliancefrontend/controllers/AuthAndSessionDataBehaviour.scala
+++ test/uk/gov/hmrc/eusubsidycompliancefrontend/controllers/AuthAndSessionDataBehaviour.scala
@@ -37,7 +37,7 @@ trait AuthAndSessionDataBehaviour { this: ControllerSpec with AuthSupport with J
   private val groupId = Some("groupIdentifier")
 
   private lazy val expectedSignInUrl: String =
-    s"$ggSignInUrl?" + s"continue=${URLEncoder.encode("/", "UTF-8")}&origin=$appName"
+    s"$ggSignInUrl?" + s"continue=${URLEncoder.encode("http://localhost/", "UTF-8")}&origin=$appName"
 
   private def identifiers(eori: EORI) = Seq(EnrolmentIdentifier("EORINumber", eori))
   private def eccEnrolments(eori: EORI) = Enrolment(key = eccEnrolmentKey, identifiers = identifiers(eori), state = "")
