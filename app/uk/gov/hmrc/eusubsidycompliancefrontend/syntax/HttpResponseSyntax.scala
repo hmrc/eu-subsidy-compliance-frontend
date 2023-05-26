b1fccc6664501421075f5c8002296dd98cca2cac
Patrick Byrne <patrick.byrne@digital.hmrc.gov.uk>
2023-05-26 14:31:30 +0100

    ESC-903 Add logging for EORI on the frontend

--- app/uk/gov/hmrc/eusubsidycompliancefrontend/syntax/HttpResponseSyntax.scala
+++ app/uk/gov/hmrc/eusubsidycompliancefrontend/syntax/HttpResponseSyntax.scala
@@ -16,30 +16,75 @@
 
 package uk.gov.hmrc.eusubsidycompliancefrontend.syntax
 
-import play.api.libs.json.Reads
+import play.api.libs.json.{JsPath, JsonValidationError, Reads}
 import uk.gov.hmrc.http.HttpResponse
 
+import scala.reflect.ClassTag
 import scala.util.{Failure, Success, Try}
 
 object HttpResponseSyntax {
 
-  implicit class HttpResponseOps(private val response: HttpResponse) extends AnyVal {
+  trait ResponseParsingLogger[A, B] {
 
-    def parseJSON[A](implicit reads: Reads[A]): Either[String, A] =
+    def logSuccess(a: B): Unit
+
+    def logValidationFailure(
+      response: HttpResponse,
+      validationFailures: collection.Seq[(JsPath, collection.Seq[JsonValidationError])]
+    ): Unit
+
+    def logParsingFailure(response: HttpResponse, throwable: Throwable): Unit
+  }
+
+  implicit class HttpResponseOps[ResponseError](private val response: HttpResponse) extends AnyVal {
+
+    /**
+      * For now logging can be handled by a type class (version of an adaptor pattern passed often implicitly, such as sort stuff)
+      *  as going from no parenthesis to some will cause a lot of call changes and I want to limit noise in this PR.
+      *
+      *  Due to GPDR etc we need to be picky what we log? Better to be safe than sorry
+      *
+      * @param reads
+      * @tparam A
+      * @return
+      */
+    def parseJSON[A](implicit
+      reads: Reads[A],
+      maybeLogger: Option[ResponseParsingLogger[ResponseError, A]] = None,
+      classTag: ClassTag[
+        A
+      ] // so we can keep track of what generic type we are using, generics gets type erased by the JVM
+    ): Either[String, A] =
       Try(response.json) match {
         case Success(jsValue) =>
           jsValue
             .validate[A]
             .fold[Either[String, A]](
-              _ =>
-                // there was JSON in the response but we couldn't read it
-                Left("Could not parse http response JSON"),
-              Right(_)
+              (validationFailures: collection.Seq[(JsPath, collection.Seq[JsonValidationError])]) =>
+                processFailedValidation(maybeLogger, validationFailures),
+              (successValue: A) => processSuccessfulParsing(maybeLogger, successValue)
             )
-        case Failure(_) =>
-          // response.json failed in this case - there was no JSON in the response
-          Left(s"Could not read http response as JSON")
+
+        case Failure(error: Throwable) =>
+          maybeLogger.foreach(_.logParsingFailure(response, error))
+          Left(s"Could not read http response as valid expected JSON format for $classTag")
       }
 
+    private def processFailedValidation[A](
+      maybeLogger: Option[ResponseParsingLogger[ResponseError, A]],
+      validationFailures: collection.Seq[(JsPath, collection.Seq[JsonValidationError])]
+    ): Left[String, Nothing] = {
+      maybeLogger.foreach(_.logValidationFailure(response, validationFailures))
+      Left("Could not parse http response JSON")
+    }
+
+    private def processSuccessfulParsing[A, B](
+      maybeLogger: Option[ResponseParsingLogger[ResponseError, A]],
+      result: A
+    ): Either[String, A] = {
+      maybeLogger.foreach(_.logSuccess(result))
+      Right(result)
+    }
   }
+
 }
