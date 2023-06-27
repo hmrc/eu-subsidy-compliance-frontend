f3ef318a9d1ba68e7f0fc1b52c282588a492de03
Patrick Byrne <patrick.byrne@digital.hmrc.gov.uk>
2023-06-27 12:19:05 +0100

    upgrade-hmrc-play-frontend for bobby rules

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,22 +10,22 @@ object AppDependencies {
   val enumeratumVersion = "1.7.0"
 
   val compile = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"    % bootStrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"            % "3.33.0-play-28",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"            % hmrcMongoVersion,
-    "org.typelevel"     %% "cats-core"                     % "2.7.0",
-    "com.chuusai"       %% "shapeless"                     % "2.3.9",
-    "uk.gov.hmrc"       %% "play-conditional-form-mapping" % "1.12.0-play-28",
-    "com.beachape"      %% "enumeratum"                    % enumeratumVersion,
-    "com.beachape"      %% "enumeratum-play-json"          % enumeratumVersion
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootStrapVersion,
+    "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.13.0-play-28",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % hmrcMongoVersion,
+    "org.typelevel" %% "cats-core" % "2.7.0",
+    "com.chuusai" %% "shapeless" % "2.3.9",
+    "uk.gov.hmrc" %% "play-conditional-form-mapping" % "1.12.0-play-28",
+    "com.beachape" %% "enumeratum" % enumeratumVersion,
+    "com.beachape" %% "enumeratum-play-json" % enumeratumVersion
   )
 
   val test = Seq(
-    "uk.gov.hmrc"         %% "bootstrap-test-play-28"  % bootStrapVersion % "test, it",
-    "uk.gov.hmrc.mongo"   %% "hmrc-mongo-test-play-28" % hmrcMongoVersion % "test, it",
-    "org.jsoup"            % "jsoup"                   % "1.14.3"         % Test,
-    "com.vladsch.flexmark" % "flexmark-all"            % "0.36.8"         % "test, it",
-    "org.scalatestplus"   %% "mockito-3-4"             % "3.2.10.0"       % "test, it",
-    "org.scalamock"       %% "scalamock"               % "5.2.0"          % Test
+    "uk.gov.hmrc" %% "bootstrap-test-play-28" % bootStrapVersion % "test, it",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-28" % hmrcMongoVersion % "test, it",
+    "org.jsoup" % "jsoup" % "1.14.3" % Test,
+    "com.vladsch.flexmark" % "flexmark-all" % "0.36.8" % "test, it",
+    "org.scalatestplus" %% "mockito-3-4" % "3.2.10.0" % "test, it",
+    "org.scalamock" %% "scalamock" % "5.2.0" % Test
   )
 }
