c8e6ec71dbaa3326d987299c364f6a34748dd9d8
“WitchellJack” <WitchellJack@users.noreply.github.com>
2023-10-03 10:46:47 +0100

    ESC-1102 - added default proxy configuration

--- app/uk/gov/hmrc/eusubsidycompliancefrontend/connectors/EuropaConnector.scala
+++ app/uk/gov/hmrc/eusubsidycompliancefrontend/connectors/EuropaConnector.scala
@@ -18,12 +18,8 @@ package uk.gov.hmrc.eusubsidycompliancefrontend.connectors
 
 import play.api.http.HeaderNames.ACCEPT
 import uk.gov.hmrc.eusubsidycompliancefrontend.models.MonthlyExchangeRate
-import uk.gov.hmrc.http.{HeaderCarrier, HttpClient}
+import uk.gov.hmrc.http.HeaderCarrier
 import uk.gov.hmrc.http.HttpReads.Implicits._
-import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
-
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.{ExecutionContext, Future}
 @Singleton
