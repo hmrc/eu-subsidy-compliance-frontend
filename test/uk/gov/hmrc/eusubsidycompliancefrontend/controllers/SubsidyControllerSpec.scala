08284be47975849a06f8d624fc305308e3b01320
ymabdulahi <ymabdulahi@gmail.com>
2024-02-05 09:23:57 +0000

    addressed pr bot comments

--- test/uk/gov/hmrc/eusubsidycompliancefrontend/controllers/SubsidyControllerSpec.scala
+++ test/uk/gov/hmrc/eusubsidycompliancefrontend/controllers/SubsidyControllerSpec.scala
@@ -175,7 +175,9 @@ class SubsidyControllerSpec
               subsidyList.select("tbody > tr > td:nth-child(3)").text() shouldBe "GB123456789012"
               subsidyList.select("tbody > tr > td:nth-child(4)").text() shouldBe "Local Authority"
               subsidyList.select("tbody > tr > td:nth-child(5)").text() shouldBe "ABC123"
-              subsidyList.select("tbody > tr > td:nth-child(6)").text() shouldBe "Remove payment, dated 1 Jan 2022, EORI number GB123456789012"
+              subsidyList
+                .select("tbody > tr > td:nth-child(6)")
+                .text() shouldBe "Remove payment, dated 1 Jan 2022, EORI number GB123456789012"
 
               subsidyList.select("tbody > tr > td:nth-child(6) > a").attr("href") shouldBe routes.SubsidyController
                 .getRemoveSubsidyClaim("Z12345")
