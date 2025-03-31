4c4f1b6286ed60382292f0c623d248490782cbd5
Liam-Macarthur <113617571+Liam-Macarthur@users.noreply.github.com>
2025-03-31 11:50:10 +0100

    Updating dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,11 +3,11 @@ import sbt._
 object AppDependencies {
 
   val bootStrapVersion = "9.11.0"
-  val hmrcMongoVersion = "2.5.0"
+  val hmrcMongoVersion = "2.6.0"
 
   val compile = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootStrapVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "11.12.0",
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "11.13.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30" % hmrcMongoVersion,
     "org.typelevel" %% "cats-core" % "2.10.0",
     "com.chuusai" %% "shapeless" % "2.3.10",
