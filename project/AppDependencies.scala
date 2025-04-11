eb74bab5a3c6f7343f862d514640310396c92e5a
Liam-Macarthur <113617571+Liam-Macarthur@users.noreply.github.com>
2025-04-11 13:58:37 +0100

    Updating dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,7 +11,7 @@ object AppDependencies {
     "uk.gov.hmrc.mongo"     %% "hmrc-mongo-play-30"                         % hmrcMongoVersion,
     "org.typelevel"         %% "cats-core"                                  % "2.10.0",
     "com.chuusai"           %% "shapeless"                                  % "2.3.10",
-    "uk.gov.hmrc"           %% "play-conditional-form-mapping-play-30"      % "3.2.0",
+    "uk.gov.hmrc"           %% "play-conditional-form-mapping-play-30"      % "3.3.0",
     "com.beachape"          %% "enumeratum"                                 % "1.7.3",
     "com.beachape"          %% "enumeratum-play-json"                       % "1.8.0"
   )
