324add59a90821f90bc5730fd3add95cf4e25fb6
lewis-nicholls <173899555+lewis-nicholls@users.noreply.github.com>
2025-06-27 13:22:46 +0100

    DDCYLS-6950: dependency upgrades (#524)
    * dependency upgrades
    
    * dependency upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,12 @@ import sbt._
 
 object AppDependencies {
 
-  val bootStrapVersion = "9.11.0"
+  val bootStrapVersion = "9.13.0"
   val hmrcMongoVersion = "2.6.0"
 
   val compile = Seq(
     "uk.gov.hmrc"           %% "bootstrap-frontend-play-30"                 % bootStrapVersion,
-    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"                 % "11.13.0",
+    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"                 % "12.6.0",
     "uk.gov.hmrc.mongo"     %% "hmrc-mongo-play-30"                         % hmrcMongoVersion,
     "org.typelevel"         %% "cats-core"                                  % "2.10.0",
     "com.chuusai"           %% "shapeless"                                  % "2.3.10",
