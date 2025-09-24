afe68d7044f6c3c62d18d183b63af406c2bc2020
lewis-nicholls <173899555+lewis-nicholls@users.noreply.github.com>
2025-09-24 08:32:42 +0100

    reverting undertaking sector page.

--- app/uk/gov/hmrc/eusubsidycompliancefrontend/controllers/UndertakingController.scala
+++ app/uk/gov/hmrc/eusubsidycompliancefrontend/controllers/UndertakingController.scala
@@ -48,7 +48,6 @@ import play.api.mvc.Call
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.{ExecutionContext, Future}
 import uk.gov.hmrc.eusubsidycompliancefrontend.journeys.UndertakingJourney.Forms.UndertakingCyaFormPage
-import uk.gov.hmrc.eusubsidycompliancefrontend.views.html.nace.lvl1.GeneralTradeUndertakingOtherPage
 
 @Singleton
 class UndertakingController @Inject() (
@@ -62,7 +61,7 @@ class UndertakingController @Inject() (
   timeProvider: TimeProvider,
   auditService: AuditService,
   aboutUndertakingPage: AboutUndertakingPage,
-  undertakingSectorPage: GeneralTradeUndertakingOtherPage,
+  undertakingSectorPage: UndertakingSectorPage,
   undertakingAddBusinessPage: UndertakingAddBusinessPage,
   override val confirmEmailPage: ConfirmEmailPage,
   override val inputEmailPage: InputEmailPage,
