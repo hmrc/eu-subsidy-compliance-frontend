ed07dae3c0fe80adaf69c7dc95f48e54e4607f0e
John Owens <john.owens@opencastsoftware.com>
2025-09-29 14:14:27 +0100

    resovled formatting issues
    # Conflicts:
    #	app/uk/gov/hmrc/eusubsidycompliancefrontend/models/types/package.scala

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,20 +6,20 @@ object AppDependencies {
   val hmrcMongoVersion = "2.6.0"
 
   val compile = Seq(
-    "uk.gov.hmrc"           %% "bootstrap-frontend-play-30"                 % bootStrapVersion,
-    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"                 % "12.8.0",
-    "uk.gov.hmrc.mongo"     %% "hmrc-mongo-play-30"                         % hmrcMongoVersion,
-    "org.typelevel"         %% "cats-core"                                  % "2.10.0",
-    "com.chuusai"           %% "shapeless"                                  % "2.3.10",
-    "uk.gov.hmrc"           %% "play-conditional-form-mapping-play-30"      % "3.3.0",
-    "com.beachape"          %% "enumeratum"                                 % "1.7.3",
-    "com.beachape"          %% "enumeratum-play-json"                       % "1.8.0"
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootStrapVersion,
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "12.8.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30" % hmrcMongoVersion,
+    "org.typelevel" %% "cats-core" % "2.10.0",
+    "com.chuusai" %% "shapeless" % "2.3.10",
+    "uk.gov.hmrc" %% "play-conditional-form-mapping-play-30" % "3.3.0",
+    "com.beachape" %% "enumeratum" % "1.7.3",
+    "com.beachape" %% "enumeratum-play-json" % "1.8.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"                 %% "bootstrap-test-play-30"         % bootStrapVersion % Test,
-    "uk.gov.hmrc.mongo"           %% "hmrc-mongo-test-play-30"        % hmrcMongoVersion % Test,
-    "org.jsoup"                   % "jsoup"                           % "1.17.2" % Test,
-    "org.scalamock"               %% "scalamock"                      % "5.2.0" % Test
+    "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootStrapVersion % Test,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % hmrcMongoVersion % Test,
+    "org.jsoup" % "jsoup" % "1.17.2" % Test,
+    "org.scalamock" %% "scalamock" % "5.2.0" % Test
   )
 }
