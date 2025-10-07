4e755ebe307c383b479a14b65b21ea9f5b3b5fd9
AthishThayalan <120419238+AthishThayalan@users.noreply.github.com>
2025-10-07 14:14:02 +0100

    Add NACE check details page with form validation - still need routing to final page and back button

--- app/uk/gov/hmrc/eusubsidycompliancefrontend/controllers/NACECheckDetailsController.scala
+++ app/uk/gov/hmrc/eusubsidycompliancefrontend/controllers/NACECheckDetailsController.scala
@@ -16,9 +16,11 @@
 
 package uk.gov.hmrc.eusubsidycompliancefrontend.controllers
 
+import play.api.data.Form
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.eusubsidycompliancefrontend.config.AppConfig
-import uk.gov.hmrc.eusubsidycompliancefrontend.controllers.routes
+import uk.gov.hmrc.eusubsidycompliancefrontend.forms.FormHelpers.formWithSingleMandatoryField
+import uk.gov.hmrc.eusubsidycompliancefrontend.models.FormValues
 import uk.gov.hmrc.eusubsidycompliancefrontend.models.types.Sector
 import uk.gov.hmrc.eusubsidycompliancefrontend.views.html.nace.ConfirmDetailsPage
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
@@ -32,15 +34,38 @@ class NACECheckDetailsController @Inject()(
                                             naceCYAView: ConfirmDetailsPage
                                           )(implicit ec: ExecutionContext, appConfig: AppConfig) extends FrontendController(mcc) {
 
+  private val confirmDetailsForm: Form[FormValues] = formWithSingleMandatoryField("confirmDetails")
+
+
   def getCheckDetails: Action[AnyContent] = Action { implicit request =>
-    // Placeholder values for rendering
     val sector = Sector.agriculture
     val naceLevel1 = "User's selection of L1"
     val naceLevel2 = "User's radio selection of L2"
     val naceLevel3 = "User's radio selection of L3"
     val naceLevel4 = "User's radio selection of L4"
-    val previous = routes.UndertakingController.getUndertakingHome
+    val previous = routes.AccountController.getAccountPage.url //back link currently routed to home.
 
-    Ok(naceCYAView(sector, naceLevel1, naceLevel2, naceLevel3, naceLevel4, previous))
+    Ok(naceCYAView(confirmDetailsForm, sector, naceLevel1, naceLevel2, naceLevel3, naceLevel4, previous))
+  }
+
+  def postCheckDetails: Action[AnyContent] = Action { implicit request =>
+    confirmDetailsForm
+      .bindFromRequest()
+      .fold(
+        formWithErrors => {
+          val sector = Sector.agriculture
+          val naceLevel1 = "User's selection of L1"
+          val naceLevel2 = "User's radio selection of L2"
+          val naceLevel3 = "User's radio selection of L3"
+          val naceLevel4 = "User's radio selection of L4"
+          val previous = routes.AccountController.getAccountPage.url
+
+          BadRequest(naceCYAView(formWithErrors, sector, naceLevel1, naceLevel2, naceLevel3, naceLevel4, previous))
+        },
+        form => {
+          // currently just reloads page upon successful submission
+          Redirect(routes.NACECheckDetailsController.getCheckDetails)
+        }
+      )
   }
 }
