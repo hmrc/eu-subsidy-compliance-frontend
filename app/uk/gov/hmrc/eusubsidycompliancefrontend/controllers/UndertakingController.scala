01deab49a9e59cdf4f0e5cd181849394acf89f8d
Liam-Macarthur <113617571+Liam-Macarthur@users.noreply.github.com>
2025-12-15 12:38:38 +0000

    removing new email status to check if responsbile for looping

--- app/uk/gov/hmrc/eusubsidycompliancefrontend/controllers/UndertakingController.scala
+++ app/uk/gov/hmrc/eusubsidycompliancefrontend/controllers/UndertakingController.scala
@@ -106,7 +106,6 @@ class UndertakingController @Inject() (
       case EmailStatus.Amend => routes.UndertakingController.getAmendUndertakingDetails
       case EmailStatus.BecomeLead => routes.BecomeLeadController.getConfirmEmail
       case EmailStatus.CYA => routes.UndertakingController.getCheckAnswers
-      case EmailStatus.New => routes.UndertakingController.getConfirmEmail
       case _ => routes.UndertakingController.getSector
     }
   }
