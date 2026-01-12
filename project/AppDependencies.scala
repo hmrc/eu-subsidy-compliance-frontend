4e6fa4d217dcee3b2f75558e0b441f7d8e1399eb
vjwatson <171040641+VJWatson@users.noreply.github.com>
2026-01-12 11:34:02 +0000

    removed print lines

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,24 +2,24 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootStrapVersion = "9.18.0"
-  val hmrcMongoVersion = "2.6.0"
+  val bootStrapVersion = "9.19.0"
+  val hmrcMongoVersion = "2.11.0"
 
   val compile = Seq(
     "uk.gov.hmrc"           %% "bootstrap-frontend-play-30"                 % bootStrapVersion,
-    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"                 % "12.8.0",
+    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"                 % "12.25.0",
     "uk.gov.hmrc.mongo"     %% "hmrc-mongo-play-30"                         % hmrcMongoVersion,
-    "org.typelevel"         %% "cats-core"                                  % "2.10.0",
-    "com.chuusai"           %% "shapeless"                                  % "2.3.10",
-    "uk.gov.hmrc"           %% "play-conditional-form-mapping-play-30"      % "3.3.0",
-    "com.beachape"          %% "enumeratum"                                 % "1.7.3",
-    "com.beachape"          %% "enumeratum-play-json"                       % "1.8.0"
+    "org.typelevel"         %% "cats-core"                                  % "2.13.0",
+    "com.chuusai"           %% "shapeless"                                  % "2.3.13",
+    "uk.gov.hmrc"           %% "play-conditional-form-mapping-play-30"      % "3.4.0",
+    "com.beachape"          %% "enumeratum"                                 % "1.9.2",
+    "com.beachape"          %% "enumeratum-play-json"                       % "1.9.2"
   )
 
   val test = Seq(
     "uk.gov.hmrc"                 %% "bootstrap-test-play-30"         % bootStrapVersion % Test,
     "uk.gov.hmrc.mongo"           %% "hmrc-mongo-test-play-30"        % hmrcMongoVersion % Test,
-    "org.jsoup"                   % "jsoup"                           % "1.17.2" % Test,
-    "org.scalamock"               %% "scalamock"                      % "5.2.0" % Test
+    "org.jsoup"                   % "jsoup"                           % "1.22.1" % Test,
+    "org.scalamock"               %% "scalamock"                      % "7.5.3" % Test
   )
 }
