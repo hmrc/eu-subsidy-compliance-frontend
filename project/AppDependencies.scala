f8a714fbb41b95458b609472b169f97ee9756482
Liam-Macarthur <113617571+Liam-Macarthur@users.noreply.github.com>
2026-04-29 11:30:25 +0100

    removing rebrand and adding new hmrc header design

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"           %% "bootstrap-frontend-play-30"                 % bootStrapVersion,
-    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"                 % "12.32.0",
+    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"                 % "13.3.0",
     "uk.gov.hmrc.mongo"     %% "hmrc-mongo-play-30"                         % hmrcMongoVersion,
     "org.typelevel"         %% "cats-core"                                  % "2.13.0",
     "com.chuusai"           %% "shapeless"                                  % "2.3.13",
