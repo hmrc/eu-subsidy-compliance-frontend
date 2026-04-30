13dffb2dd8a45e749fb046aedf209c0a3950707b
AthishThayalan <120419238+AthishThayalan@users.noreply.github.com>
2026-04-30 15:20:41 +0100

    Add need-registration-number-business page

---
+++ app/uk/gov/hmrc/eusubsidycompliancefrontend/controllers/NeedRegistrationNumberBusinessController.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.eusubsidycompliancefrontend.controllers
+
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.eusubsidycompliancefrontend.actions.ActionBuilders
+import uk.gov.hmrc.eusubsidycompliancefrontend.config.AppConfig
+import uk.gov.hmrc.eusubsidycompliancefrontend.views.html.NeedRegistrationNumberBusinessPage
+
+import javax.inject.{Inject, Singleton}
+import scala.concurrent.Future
+
+@Singleton
+class NeedRegistrationNumberBusinessController @Inject() (
+  mcc: MessagesControllerComponents,
+  actionBuilders: ActionBuilders,
+  needRegistrationNumberBusinessPage: NeedRegistrationNumberBusinessPage
+)(implicit
+  val appConfig: AppConfig
+) extends BaseController(mcc) {
+
+  import actionBuilders._
+
+  def showPage: Action[AnyContent] = enrolled.async { implicit request =>
+    Future.successful(Ok(needRegistrationNumberBusinessPage(request.eoriNumber)))
+  }
+
+}
--- conf/messages.cy
+++ conf/messages.cy
@@ -14,3 +14,16 @@ date.10=Hydref
 date.11=Tachwedd
 date.12=Rhagfyr
 
+
+needRegistrationNumberBusiness.title=We need a registration number for your business
+needRegistrationNumberBusiness.eoriNumber=EORI number: {0}
+needRegistrationNumberBusiness.p1=We could not find a registration number for the business linked to your EORI number.
+needRegistrationNumberBusiness.p2=Due to a change in EU regulations, you need to provide one to keep using this service.
+needRegistrationNumberBusiness.p3=This must be a company registration number (CRN), if the business has one. If it does not, you can provide its VAT registration number, National Insurance number or charity number.
+needRegistrationNumberBusiness.whatYouNeedToDo.heading=What you need to do
+needRegistrationNumberBusiness.whatYouNeedToDo.p1=Email <a class="govuk-link" href="mailto:customs.duty-waivers@hmrc.gov.uk">customs.duty-waivers@hmrc.gov.uk</a> and include:
+needRegistrationNumberBusiness.whatYouNeedToDo.bullet1=your EORI number
+needRegistrationNumberBusiness.whatYouNeedToDo.bullet2=the business's registration number
+needRegistrationNumberBusiness.afterWeGetYourEmail.heading=After we get your email
+needRegistrationNumberBusiness.afterWeGetYourEmail.p1=A caseworker will contact you within 5 working days. They will help update your details.
+needRegistrationNumberBusiness.afterWeGetYourEmail.p2=Once this is complete, we'll email you a confirmation. You can then sign in and use the service.
