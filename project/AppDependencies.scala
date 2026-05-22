003a1012d279593369a1c9931de29f937b835b8f
Dipen Lakhani <13121210+dips7189@users.noreply.github.com>
2026-05-22 15:21:41 +0100

    DDCYLS-8165 Upgrading Dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"           %% "bootstrap-frontend-play-30"                 % bootStrapVersion,
-    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"                 % "13.5.0",
+    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"                 % "13.6.0",
     "uk.gov.hmrc.mongo"     %% "hmrc-mongo-play-30"                         % hmrcMongoVersion,
     "org.typelevel"         %% "cats-core"                                  % "2.13.0",
     "com.chuusai"           %% "shapeless"                                  % "2.3.13",
