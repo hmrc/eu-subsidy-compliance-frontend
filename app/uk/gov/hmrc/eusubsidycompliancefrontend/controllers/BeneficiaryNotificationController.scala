dd690880fa7beb4ea93b15e94ffd049c9edb991a
Liam-Macarthur <113617571+Liam-Macarthur@users.noreply.github.com>
2026-06-26 12:38:07 +0100

    adding nav for hmrc email and ben notification pages

--- app/uk/gov/hmrc/eusubsidycompliancefrontend/controllers/BeneficiaryNotificationController.scala
+++ app/uk/gov/hmrc/eusubsidycompliancefrontend/controllers/BeneficiaryNotificationController.scala
@@ -20,28 +20,28 @@ import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import uk.gov.hmrc.eusubsidycompliancefrontend.actions.ActionBuilders
 import uk.gov.hmrc.eusubsidycompliancefrontend.config.AppConfig
 import uk.gov.hmrc.eusubsidycompliancefrontend.syntax.FutureSyntax.FutureOps
-import uk.gov.hmrc.eusubsidycompliancefrontend.views.html.ConfirmedMemberBusinessDetailsPage
+import uk.gov.hmrc.eusubsidycompliancefrontend.views.html.BenNotificationPage
 
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.ExecutionContext
 
 @Singleton
-class MemberBusinessDetailsController @Inject() (
-  mcc: MessagesControllerComponents,
-  actionBuilders: ActionBuilders,
-  memberBusinessDetailsPage: ConfirmedMemberBusinessDetailsPage
-)(implicit
-  val appConfig: AppConfig,
-  val executionContext: ExecutionContext
-) extends BaseController(mcc) {
+class BeneficiaryNotificationController @Inject() (
+                                                    mcc: MessagesControllerComponents,
+                                                    actionBuilders: ActionBuilders,
+                                                    beneficiaryNotificationPage: BenNotificationPage
+                                                  )(implicit
+                                                    val appConfig: AppConfig,
+                                                    val executionContext: ExecutionContext
+                                                  ) extends BaseController(mcc) {
 
   import actionBuilders._
 
   def showPage(): Action[AnyContent] = enrolled.async { implicit request =>
-    Ok(memberBusinessDetailsPage()).toFuture
+    Ok(beneficiaryNotificationPage(routes.MemberBusinessDetailsController.showPage())).toFuture
   }
 
   def submitPage(): Action[AnyContent] = enrolled.async { implicit request =>
-    Redirect(routes.BeneficiaryNotificationController.showPage()).toFuture
+    Redirect(routes.AccountController.getAccountPage).toFuture
   }
 }
