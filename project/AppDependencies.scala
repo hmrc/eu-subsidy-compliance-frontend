3279588da0c51173861bc03ae6ea833abe19839b
bhanu-nov1989 <177983246+bhanu-nov1989@users.noreply.github.com>
2026-07-16 11:47:21 +0100

    upgrade the dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"           %% "bootstrap-frontend-play-30"                 % bootStrapVersion,
-    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"                 % "13.8.0",
+    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"                 % "13.9.0",
     "uk.gov.hmrc.mongo"     %% "hmrc-mongo-play-30"                         % hmrcMongoVersion,
     "org.typelevel"         %% "cats-core"                                  % "2.13.0",
     "uk.gov.hmrc"           %% "play-conditional-form-mapping-play-30"      % "3.5.0",
