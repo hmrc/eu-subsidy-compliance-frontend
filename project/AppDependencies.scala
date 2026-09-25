3f7b0cb9102d69e6e944f08bb79794f980627a7a
kreddam <194923482+kreddam@users.noreply.github.com>
2026-09-25 13:11:05 +0100

    DDCYLS-6725 Removal of Unused Imports scala format check

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,11 +3,11 @@ import sbt.*
 object AppDependencies {
 
   val bootStrapVersion = "10.8.0"
-  val hmrcMongoVersion = "2.13.0"
+  val hmrcMongoVersion = "2.14.0"
 
   val compile = Seq(
     "uk.gov.hmrc"           %% "bootstrap-frontend-play-30"                 % bootStrapVersion,
-    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"                 % "13.9.0",
+    "uk.gov.hmrc"           %% "play-frontend-hmrc-play-30"                 % "13.14.0",
     "uk.gov.hmrc.mongo"     %% "hmrc-mongo-play-30"                         % hmrcMongoVersion,
     "org.typelevel"         %% "cats-core"                                  % "2.13.0",
     "uk.gov.hmrc"           %% "play-conditional-form-mapping-play-30"      % "3.5.0",
